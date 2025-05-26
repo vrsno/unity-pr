f984bc2f5b2fb9d88c61dd00fcf3f7e0eb8e01d7
sinname <nexxuz2017@gmail.com>
2025-05-26 09:42:45 -0500

    v4

--- index2.cs
+++ index2.cs
@@ -6,13 +6,21 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            int valor = 3;
+            // int valor = 3;
+            // int valor2 = 4;
+            // var suma = valor + valor2;
+            // var resta = valor - valor2;
+            // var multiplicacion = valor * valor2;
+            // var division = valor / valor2;
+
+            int valor1 = 34;
             int valor2 = 4;
-            var suma = valor + valor2;
-            var resta = valor - valor2;
-            var multiplicacion = valor * valor2;
-            var division = valor / valor2;
-            Console.WriteLine(suma, " {0} {1} {2} {3}", resta, multiplicacion, division);
+            //valor1 += valor2; // Suma
+            //valor1 -= valor2; // resta
+            //valor1 *= valor2; // Multiplicación
+            //valor1 /= valor2; // División
+            valor1 %= valor2; // Módulo-residuo
+            Console.WriteLine("Valor1 después de asignacion: " + valor1);
             Console.ReadLine();
         }
     }
