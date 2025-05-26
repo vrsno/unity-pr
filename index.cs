00a35fbce467d6e6325a032911f2a6925f2ac688
sinname <nexxuz2017@gmail.com>
2025-05-26 09:24:15 -0500

    v2

--- index.cs
+++ index.cs
@@ -10,9 +10,14 @@ namespace HelloWorld
             double doble = 2.5;
             string nombre = "Unity";
             string nombres = "Unity Unreal Godot";
-            decimal decimals = 2.07m;
+            decimal decimals = 2.07M;
             float flotante = 3.14f;
-            Console.WriteLine(nombres + " {0} {1} {2} {3}", year, doble, nombre, decimals);
+            bool valor = true;
+            char letra = 'A';
+            Decimal de = 28.9M;
+            Boolean valor2 = false;
+            // estructura Bool Decimal Char String Int Double Float
+            Console.WriteLine(nombres + " {0} {1} {2} {3}", year, doble, nombre, decimals, valor, letra, de, valor2);
             console.ReadLine();
         }
     }
