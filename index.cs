e189f8035f1da338c929b4234f7089aacf560bd0
sinname <nexxuz2017@gmail.com>
2025-05-26 09:15:21 -0500

    v1

---
+++ index.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int year = Convert.ToInt16("120");
+            double doble = 2.5;
+            string nombre = "Unity";
+            string nombres = "Unity Unreal Godot";
+            decimal decimals = 2.07m;
+            float flotante = 3.14f;
+            Console.WriteLine(nombres + " {0} {1} {2} {3}", year, doble, nombre, decimals);
+            console.ReadLine();
+        }
+    }
+}
